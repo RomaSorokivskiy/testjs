fd2354b2cac3c43357df8ff8a7ec6fd56dd8f4ac
sorok <sorokivroma@gmail.com>
2021-12-06 22:13:31 +0300

    commit

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-
 let lineW = document.getElementById('lineW').value;
 let lineC = document.getElementById('lineC').value;
 
-lineW = 2;
+const canvas = document.getElementById('canvas');
 
-var myColor;
+let ctx = canvas.getContext('2d');
+ctx.lineCap = 'round';
+let paint = false;
+
+
+function init() {
+
+  canvas.addEventListener("mousedown", Down);
+  canvas.addEventListener("mouseup", Up);
+  canvas.addEventListener("mousemove", Move);
+
+  function Down(e) {
+    paint = true;
+    ctx.beginPath();
+    ctx.moveTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+  }
+
+  function Up(e) {
+    paint = false;
+    ctx.closePath();
+  }
+
+  function Move(e) {
+    if (!paint) return;
+    ctx.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+    ctx.stroke();
+  }
+}
+
+let myColor;
+
 document.getElementById('lineC').oninput = function() {
   myColor = this.value;
 };
 
-canvas.onmousedown = function(event) {
-  canvas.onmousemove = function(event) {
-    var x = event.offsetX;
-    var y = event.offsetY;
-    ctx.fillStyle = myColor;
-    ctx.fillRect(x - lineW, y -lineW, lineW,lineW);
-    ctx.fill();
-  };
+document.querySelector('form.canvas-attrs').addEventListener('change', setStyle);
+function setStyle() {
+  ctx.strokeStyle = myColor;
+  ctx.lineWidth = this.lineW.value;
+}
 
-  canvas.onmouseup = function() {
-    canvas.onmousemove = null;
-  };
-};
-    function done(){
-      lineW = document.getElementById('lineW').value;
-      lineC = document.getElementById('lineC').value;
-    }
-    function clearCanvas(){
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
+function clearCanvas(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+init();
